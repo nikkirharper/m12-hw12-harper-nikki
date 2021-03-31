5ef5e93431a30c18a2762b93c328d65dd1c75435
Nikki Harper <nicoleharper@ufl.edu>
2021-03-30 19:40:45 -0600

    Add random quote button and functionality

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 //Random Will Ferrell Quotes
-const quotes = [
+var button = document.getElementById("button"); 
+var quotes = [
     "He's an angry elf!” – Will Ferrell in Elf",
     "Well, um, actually a pretty nice little Saturday, we’re going to go to Home Depot. Yeah, buy some wallpaper, maybe get some flooring, stuff like that. Maybe Bed, Bath, & Beyond, I don’t know, I don’t know if we’ll have enough time. -Will Ferrell in Old School", 
     "I swear, I’m so pissed off at my mom. As soon as she’s of age, I’m putting her in a home. – Will Ferrell in Step Brothers",
@@ -13,3 +14,17 @@ const quotes = [
     "Son of nutcracker – Will Ferrell in Elf",
     "I'm Ron Burgundy? -Will Ferrell in Ron Burgundy",
     ];
+
+
+    function randomizeQuotes() {
+        var i, j, k;
+          for (i = quotes.length -1; i > 0; i--) {
+            j = Math.floor(Math.random() * i)
+            k = quotes[i]
+            quotes[i] = quotes[j]
+            quotes[j] = k
+          }
+          document.getElementById("quote").innerHTML = quotes[0];
+        }; 
+    
+        button.addEventListener("click", randomizeQuotes); 
